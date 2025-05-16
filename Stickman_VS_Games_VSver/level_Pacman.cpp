6b719d209a1d7556d2f5cf9a2c8830555bd6b95d
FlySparkle <2795077456@qq.com>
2025-05-16 09:04:16 +0800

    只有man没有pac

--- Stickman_VS_Games_VSver/level_Pacman.cpp
+++ Stickman_VS_Games_VSver/level_Pacman.cpp
@@ -1,33 +1,33 @@
 #include <windows.h>
 #include <iostream>
 #include "level_Pacman.h"
-GamePacman::GamePacman() : rng(time(nullptr))
-{
-}
+
+GamePacman::GamePacman() : rng(time(nullptr)) {}
+
 void GamePacman::initGrid()
 {
+    // 地图
     int map[20][20] = {
-            {WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, PELLET, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, PELLET, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, PELLET, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, PELLET, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, WALLLL, WALLLL, PELLET, PELLET, PELLET, PELLET, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL},
-            {WALLLL, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL},
-            {WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, PELLET, WALLLL},
-            {WALLLL, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, PELLET, WALLLL},
-            {WALLLL, PELLET, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL},
-            {WALLLL, PELLET, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL},
-            {WALLLL, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, PELLET, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL},
-            {WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL, WALLLL}
-    };
+        {WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL},
+        {WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, WALL, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, FOOD, WALL},
+        {WALL, FOOD, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, FOOD, WALL},
+        {WALL, FOOD, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, FOOD, WALL},
+        {WALL, FOOD, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, WALL},
+        {WALL, FOOD, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL},
+        {WALL, FOOD, FOOD, FOOD, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, WALL, WALL},
+        {WALL, FOOD, WALL, WALL, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL},
+        {WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL},
+        {WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, WALL, WALL},
+        {WALL, FOOD, WALL, FOOD, WALL, WALL, FOOD, WALL, WALL, FOOD, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL},
+        {WALL, FOOD, WALL, FOOD, WALL, WALL, FOOD, FOOD, FOOD, FOOD, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, WALL},
+        {WALL, FOOD, WALL, FOOD, WALL, WALL, WALL, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, WALL, WALL, FOOD, WALL},
+        {WALL, FOOD, FOOD, FOOD, WALL, WALL, WALL, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, WALL, WALL, FOOD, WALL},
+        {WALL, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, WALL, WALL, FOOD, WALL},
+        {WALL, FOOD, FOOD, FOOD, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL, FOOD, WALL},
+        {WALL, FOOD, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, WALL, WALL, WALL, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, WALL},
+        {WALL, FOOD, WALL, WALL, FOOD, WALL, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, WALL, FOOD, WALL, WALL, WALL, WALL, WALL},
+        {WALL, FOOD, FOOD, FOOD, FOOD, WALL, WALL, WALL, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, FOOD, WALL, WALL, WALL, WALL, WALL},
+        {WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL, WALL} };
 
     for (int i = 0; i < GRID_SIZE; ++i)
     {
@@ -37,21 +37,22 @@ void GamePacman::initGrid()
         }
     }
 }
+
 void GamePacman::initMovers()
 {
     player.x = 1;
     player.y = 18;
     player.direction = Direction::RIGHT;
-    player.speed = 3;
+    player.speed = 1;
     ghost[0].x = 18;
     ghost[0].y = 1;
     ghost[0].direction = Direction::LEFT;
-    ghost[0].speed = 2;
+    ghost[0].speed = 1;
     ghost[0].live = 1;
     ghost[1].x = 18;
     ghost[1].y = 16;
     ghost[1].direction = Direction::LEFT;
-    ghost[1].speed = 2;
+    ghost[1].speed = 1;
     ghost[1].live = 1;
     ghost[2].x = 1;
     ghost[2].y = 1;
@@ -59,13 +60,23 @@ void GamePacman::initMovers()
     ghost[2].speed = 1;
     ghost[2].form = 0;
     ghost[2].live = 1;
-    //结构体整体赋值
-}void GamePacman::updateGrid() {
+    // 结构体整体赋值
+}
+void GamePacman::initGame()
+{
+    gameOver = false;
+    score = 0;
+    initGrid();
+    initMovers();
+    updateGrid();
+}
+void GamePacman::updateGrid()
+{
     // 清除之前的玩家和鬼魂位置
-    grid[player.y][player.x] = EMPTYY;
-    grid[ghost[0].y][ghost[0].x] = EMPTYY;
-    grid[ghost[1].y][ghost[1].x] = EMPTYY;
-    grid[ghost[2].y][ghost[2].x] = EMPTYY;
+    grid[player.old_y][player.old_x] = EMPTY;
+    grid[ghost[0].old_y][ghost[0].old_x] = EMPTY;
+    grid[ghost[1].old_y][ghost[1].old_x] = EMPTY;
+    grid[ghost[2].old_y][ghost[2].old_x] = EMPTY;
 
     // 更新新的位置
     grid[player.y][player.x] = PLAYER;
@@ -73,6 +84,7 @@ void GamePacman::initMovers()
     grid[ghost[1].y][ghost[1].x] = GHOST2;
     grid[ghost[2].y][ghost[2].x] = GHOST3;
 }
+
 bool GamePacman::processInput(char key)
 {
     Direction newDir = player.direction;
@@ -99,53 +111,37 @@ bool GamePacman::processInput(char key)
     player.direction = newDir;
     return true;
 }
+
 void GamePacman::movePlayer()
 {
+    player.old_x = player.x;
+    player.old_y = player.y;
     switch (player.direction)
     {
     case Direction::RIGHT:
-        if (grid[player.y][player.x + 1] != WALLLL)
+        if (grid[player.y][player.x + 1] != WALL)
             player.x += player.speed;
         break;
     case Direction::UP:
-        if (grid[player.y - 1][player.x] != WALLLL)
+        if (grid[player.y - 1][player.x] != WALL)
             player.y -= player.speed;
         break;
     case Direction::LEFT:
-        if (grid[player.y][player.x - 1] != WALLLL)
+        if (grid[player.y][player.x - 1] != WALL)
             player.x -= player.speed;
         break;
     case Direction::DOWN:
-        if (grid[player.y + 1][player.x] != WALLLL)
+        if (grid[player.y + 1][player.x] != WALL)
             player.y += player.speed;
         break;
     }
     updateGrid();
-
 }
-//main
-void GamePacman::initGame()
-{
-    gameOver = false;
-    score = 0;
-    initGrid();
-    initMovers();
-    updateGrid();
-}
-
 
 void GamePacman::update(char key)
 {
     if (processInput(key))
         movePlayer();
-}
-int GamePacman::getScore() const
-{
-    return 0;
-}
-GameState GamePacman::state() const
-{
-    return gameOver ? GameState::GameOver : GameState::Running;
 }
 
 vector<vector<int>> GamePacman::getGrid() const
@@ -160,18 +156,28 @@ vector<vector<int>> GamePacman::getGrid() const
     }
     return gridCopy;
 }
+//待修改
+int GamePacman::getScore() const
+{
+    return 0;
+}
+
+GameState GamePacman::state() const
+{
+    return gameOver ? GameState::GameOver : GameState::Running;
+}
 // Test methods
 void GamePacman::display(const vector<vector<int>>& grid, int size) const
 {
 
-    //    system("cls");
+    system("cls");
     for (int i = 0; i < size; ++i)
     {
         for (int j = 0; j < size; ++j)
         {
-            if (grid[i][j] == WALLLL)
-                cout << "# ";
-            else if (grid[i][j] == PELLET)
+            if (grid[i][j] == WALL)
+                cout << "  "; // 空格
+            else if (grid[i][j] == FOOD)
                 cout << "* ";
             else if (grid[i][j] == PLAYER)
                 cout << "@ ";
@@ -186,18 +192,17 @@ void GamePacman::display(const vector<vector<int>>& grid, int size) const
         }
         cout << endl;
     }
-
-
-    cout << "\n使用WASD键移动, ESC退出\n";
-
 }
 
-void GamePacman::startGame() {
+void GamePacman::startGame()
+{
     initGame();
     display(getGrid(), 20);
 
-    while (!gameOver) {
-        if (_kbhit()) {
+    while (!gameOver)
+    {
+        if (_kbhit())
+        {
             char input = _getch();
             if (input == 'q')
                 break;
@@ -205,7 +210,8 @@ void GamePacman::startGame() {
             display(getGrid(), 20);
         }
         // 自更新
-        else {
+        else
+        {
             update(' ');
             display(getGrid(), 20);
         }
